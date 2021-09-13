8844f53d714975d74a413d19a422efc7a619ad13
start-point <83851349+start-point@users.noreply.github.com>
2021-09-13 23:28:34 +0800

    day01

--- 俩数之和.js
+++ 俩数之和.js
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-<script>
     const nums = [2, 7, 11, 15];
     const target = 9;
 
@@ -40,6 +26,3 @@
     }
 
     console.log(Nt(nums, target))
-</script>
-
-</html>
