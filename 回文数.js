71247da487f2543f2b29d4fc4776dbff77130f23
start-point <83851349+start-point@users.noreply.github.com>
2021-09-13 23:29:52 +0800

    day01

--- 回文数.js
+++ 回文数.js
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-<script>
     const num = 1221;
     const isPalindrome = (x) => {
         let num = x;
@@ -31,6 +17,3 @@
 
     }
     console.log(isPalindrome(num));
-</script>
-
-</html>
